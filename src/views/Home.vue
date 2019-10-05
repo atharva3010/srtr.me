259563e0bbb84dddbea65ff7e65773f0d61d3ebc
Atharva Sharma <mailatharvasharma@gmail.com>
2019-10-05 08:48:50 +0530

    chore: some more fixes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,66 +8,91 @@
             src="../assets/undraw.png",
             width="400px",
             height="400px")
-          h1.is-size-1-desktop.is-size-2-tablet.is-size-3-mobile
-            span.is-family-code.has-text-primary.has-text-weight-bold {shrivl} 
-            | your long 
+          h1.title.is-family-monospace.is-size-1-desktop.is-size-2-tablet.is-size-3-mobile
+            span.is-family-code.has-text-primary.has-text-weight-bold {shrink} 
+            span.has-text-grey-dark your long 
             span.has-text-weight-bold.has-text-primary URL(s)
-          br
-          p.has-text-grey.has-text-left
-            span.has-text-primary.has-text-weight-bold Shrivl 
+          p.has-text-grey-dark.has-text-left
+            span.has-text-weight-bold srtr.me 
             | is a 
-            span.has-text-primary.has-text-weight-bold URL shortening service 
+            span.has-text-weight-bold URL shortening service 
             | made as a minor project by 
-            span.has-text-primary.has-text-weight-bold Atharva Sharma 
+            span.has-text-weight-bold Atharva Sharma 
             | and 
-            span.has-text-primary.has-text-weight-bold Nishank Gupta 
+            span.has-text-weight-bold Nishank Gupta 
             | of 
-            span.has-text-primary.has-text-weight-bold CSE 3rd year, Batch-A, MITS Gwalior. 
-            | It utilises the Firebase Dynamic Links api to shrink long URLs to short URLs, you can find the complete project source code
-            a.has-text-primary.has-text-weight-bold(href="https://github.com/atharva3010/shrivl") 
+            span.has-text-weight-bold CSE 3rd year, Batch-A, MITS Gwalior. 
+            | It utilises the Firebase Dynamic Links api to shrink long URLs to short URLs and Vue.js on the client side, you can find the complete project source code 
+            a.has-text-weight-bold.has-text-primary(href="https://github.com/atharva3010/srtr.me") 
               | here.
-          br
-          b-field(
-            label="Long URL",
-            label-position="on-border",
-            custom-class="has-text-primary")
-            b-input(
-              v-model="longURL"
-              type="text",
-              placeholder="Enter long URL here...",
-              expanded,
-              size="is-medium")
-          b-field(
-            label="Short URL",
-            label-position="on-border",
-            custom-class="has-text-primary"
-            v-if="shortURL")
-            b-input(
-              id="shortURL",
-              v-model="shortURL"
-              type="text",
-              expanded,
-              size="is-medium")
-            b-tooltip(
-              position="is-top",
-              label="Copy link")
-              b-button.is-medium(
-                @click="copyLink()",
-                aria-label="Copy link",)
+          div.fields
+            b-field(
+              label="Long URL",
+              label-position="on-border",
+              custom-class="has-text-primary")
+              b-input(
+                v-model="longURL"
+                type="text",
+                placeholder="Enter long URL here...",
+                expanded,
+                size="is-medium")
+              b-tooltip(
+                v-if="longURL"
+                position="is-top",
+                label="Clear URL")
+                b-button.is-medium(
+                  @click="clearFields()",
+                  aria-label="Clear URL",)
+                  b-icon(
+                    pack="fas",
+                    icon="times",
+                    size="is-small")
+            b-field(
+              label="Short URL",
+              label-position="on-border",
+              custom-class="has-text-primary"
+              v-if="shortURL")
+              b-input(
+                id="shortURL",
+                v-model="shortURL"
+                type="text",
+                expanded,
+                size="is-medium")
+              b-tooltip(
+                position="is-top",
+                label="Copy link")
+                b-button.is-medium(
+                  @click="copyLink()",
+                  aria-label="Copy link",)
+                  b-icon(
+                    pack="far",
+                    icon="copy",
+                    size="is-small")
+          .columns.reverse-row-order 
+            .column
+              b-button.is-primary.is-medium.is-fullwidth.is-size-5-mobile(
+                @click="shortURL ? copyLink() : shrinkURL()")
+                span(v-if="!shortURL") Shrink URL!
+                span(v-if="shortURL") Copy Link!
                 b-icon(
+                  v-if="!shortURL",
+                  pack="fas",
+                  icon="arrow-right",
+                  size="is-small")
+                b-icon(
+                  v-if="shortURL",
                   pack="far",
                   icon="copy",
                   size="is-small")
-
-          button.button.is-primary.is-medium.is-fullwidth.is-size-5-mobile(
-            @click="shrivlURL()"
-          )
-            span(v-if="!shortURL") Generate short URL!
-            span(v-if="shortURL") Shrivl another URL!
-            b-icon(
-              pack="fas",
-              icon="arrow-right",
-              size="is-small")
+            .column(v-if="shortURL")
+              b-button.is-primary.is-medium.is-fullwidth.is-size-5-mobile(
+                @click="shrinkURL()",
+                outlined)
+                span Shrink another URL
+                b-icon(
+                  pack="fas",
+                  icon="arrow-right",
+                  size="is-small")
           
 </template>
 
@@ -83,7 +108,7 @@ export default {
     };
   },
   methods: {
-    shrivlURL() {
+    shrinkURL() {
       return new Promise((resolve, reject) => {
         this.$http
           .post(
@@ -103,9 +128,9 @@ export default {
               this.shortURL = response.data.shortLink;
               this.$buefy.snackbar.open({
                 message: `Short URL is ${response.data.shortLink}`,
-                actionText: "Copy link",
                 type: "is-primary",
-                position: "is-top",
+                position: "is-bottom",
+                actionText: "Copy link",
                 onAction: () => {
                   this.copyLink();
                 }
@@ -113,24 +138,56 @@ export default {
               resolve(response.data);
             },
             error => {
-              this.$buefy.toast.open({
-                message: error,
-                type: "is-danger"
+              this.$buefy.snackbar.open({
+                message: error.message,
+                type: "is-danger",
+                position: "is-bottom",
+                actionText: "Retry",
+                onAction: () => {
+                  this.clearFields();
+                }
               });
               reject(error);
             }
           );
       });
     },
+    clearFields() {
+      this.longURL = "";
+      this.shortURL = "";
+    },
     copyLink() {
       var copyText = document.getElementById("shortURL");
       copyText.select();
       document.execCommand("copy");
-      this.$buefy.toast.open({
-        message: "Link copied to clipboard!",
-        queue: false
+      this.$buefy.snackbar.open({
+        message: "Link copied!",
+        queue: false,
+        type: "is-primary",
+        position: "is-bottom",
+        actionText: "Shrink another URL",
+        onAction: () => {
+          this.clearFields();
+        }
       });
     }
   }
 };
 </script>
+
+<style scoped>
+.fields {
+  padding-top: 24px;
+  padding-bottom: 24px;
+}
+.section {
+  padding-top: 12px;
+  padding-bottom: 12px;
+}
+@media only screen and (min-width: 769px) {
+  .reverse-row-order {
+    display: flex;
+    flex-direction: row-reverse;
+  }
+}
+</style>
